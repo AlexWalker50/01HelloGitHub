2bbba5a0c9495ba72c5d0c5dca478eb9cf7f2557
Alex Walker <walker50@hotmail.com>
2019-02-15 10:14:49 -0800

    Add project files.

---
+++ Console_01HelloGitHub/ProgramHello01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_01HelloGitHub
+{
+    class ProgramHello01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nBegin ProgramHello01\n");
+
+            QueueCaller();
+
+            Console.WriteLine("\nEND");
+            Console.ReadKey();
+        }
+        //
+        public static void QueueCaller()
+        {
+            Console.WriteLine("\nQueueCaller()");
+        }
+
+    }
+}
